9b7f519fe0b7a72c8711d09d500cceb5b123c19c
sjw0721 <0721sjw@gmail.com>
2019-02-20 22:58:51 +0800

    add

--- src/main/java/com/sjw/permission/common/SpringExceptionResolver.java
+++ src/main/java/com/sjw/permission/common/SpringExceptionResolver.java
@@ -1,43 +1,46 @@
-//package com.sjw.permission.common;
-//
-//import com.mmall.exception.ParamException;
-//import com.mmall.exception.PermissionException;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.servlet.HandlerExceptionResolver;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//@Slf4j
-//public class SpringExceptionResolver implements HandlerExceptionResolver {
-//
-//    @Override
-//    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-//        String url = request.getRequestURL().toString();
-//        ModelAndView mv;
-//        String defaultMsg = "System error";
-//
-//        // 这里我们要求项目中所有请求json数据，都使用.json结尾
-//        if (url.endsWith(".json")) {
-//            if (ex instanceof PermissionException || ex instanceof ParamException) {
-//                JsonData result = JsonData.fail(ex.getMessage());
-//                mv = new ModelAndView("jsonView", result.toMap());
-//            } else {
-//                log.error("unknown json exception, url:" + url, ex);
-//                JsonData result = JsonData.fail(defaultMsg);
-//                mv = new ModelAndView("jsonView", result.toMap());
-//            }
-//        } else if (url.endsWith(".page")){ // 这里我们要求项目中所有请求page页面，都使用.page结尾
-//            log.error("unknown page exception, url:" + url, ex);
-//            JsonData result = JsonData.fail(defaultMsg);
-//            mv = new ModelAndView("exception", result.toMap());
-//        } else {
-//            log.error("unknow exception, url:" + url, ex);
-//            JsonData result = JsonData.fail(defaultMsg);
-//            mv = new ModelAndView("jsonView", result.toMap());
-//        }
-//
-//        return mv;
-//    }
-//}
+package com.sjw.permission.common;
+
+import com.sjw.permission.exception.ParamException;
+import com.sjw.permission.exception.PermissionException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class SpringExceptionResolver implements HandlerExceptionResolver {
+    private static final String END_JSON = ".json";
+    private static final String END_PAGE = ".page";
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        String url = request.getRequestURL().toString();
+        ModelAndView mv;
+        String defaultMsg = "System error";
+
+        // 这里我们要求项目中所有请求json数据，都使用.json结尾
+        if (url.endsWith(END_JSON)) {
+            if (ex instanceof PermissionException || ex instanceof ParamException) {
+                JsonData result = JsonData.fail(ex.getMessage());
+                mv = new ModelAndView("jsonView", result.toMap());
+            } else {
+                log.error("unknown json exception, url:" + url, ex);
+                JsonData result = JsonData.fail(defaultMsg);
+                mv = new ModelAndView("jsonView", result.toMap());
+            }
+        } else if (url.endsWith(END_PAGE)) {
+            // 这里我们要求项目中所有请求page页面，都使用.page结尾
+            log.error("unknown page exception, url:" + url, ex);
+            JsonData result = JsonData.fail(defaultMsg);
+            mv = new ModelAndView("exception", result.toMap());
+        } else {
+            log.error("unknow exception, url:" + url, ex);
+            JsonData result = JsonData.fail(defaultMsg);
+            mv = new ModelAndView("jsonView", result.toMap());
+        }
+
+        return mv;
+    }
+}
