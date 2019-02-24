6b2bf1803374692e269fa4a80dcead7c4239bc5f
sjw9507 <871123091@qq.com>
2019-02-25 00:19:59 +0800

    add

--- src/main/java/com/sjw/permission/Application.java
+++ src/main/java/com/sjw/permission/Application.java
@@ -1,16 +1,16 @@
 package com.sjw.permission;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author sjw
  */
 @SpringBootApplication
 @EnableTransactionManagement
-@MapperScan(basePackages = {})
+@MapperScan(basePackages = {"com.sjw.permission.modules.*.dao","com.sjw.permission.modules.*.mapper"})
 public class Application {
 
     public static void main(String[] args) {
