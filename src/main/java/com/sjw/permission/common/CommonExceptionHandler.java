5d78821bfb21388f71fa571624f04113c3ba3b63
sjw9507 <871123091@qq.com>
2019-03-17 23:23:35 +0800

    add

--- src/main/java/com/sjw/permission/common/CommonExceptionHandler.java
+++ src/main/java/com/sjw/permission/common/CommonExceptionHandler.java
@@ -3,6 +3,7 @@ package com.sjw.permission.common;
 import com.sjw.permission.common.util.ErrorUtil;
 import com.sjw.permission.common.util.ResponseErrorUtils;
 import com.sjw.permission.common.vo.Result;
+import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,6 +22,20 @@ import javax.servlet.http.HttpServletResponse;
 @ControllerAdvice
 public class CommonExceptionHandler {
 
+    /**
+     * 拦截MethodArgumentNotValidException类的异常
+     *
+     * @param e exception
+     * @return Result
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public Result exceptionHandler(MethodArgumentNotValidException e) throws NullPointerException {
+        FieldError fieldError = e.getBindingResult().getFieldError();
+        assert fieldError != null;
+        String defaultMessage = fieldError.getDefaultMessage();
+        return Result.genFail(defaultMessage);
+    }
 
     /**
      * @param e exception
@@ -29,27 +44,15 @@ public class CommonExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ModelAndView defaultExceptionHandler(HttpServletRequest req, HttpServletResponse resp, Exception e) {
+        if (e instanceof BindException) {
+            FieldError fieldError = ((BindException) e).getBindingResult().getFieldError();
+            assert fieldError != null;
+            String defaultMessage = fieldError.getDefaultMessage();
+            ResponseErrorUtils.sendJsonError(404, defaultMessage, resp);
+        }
+
         ResponseErrorUtils.sendJsonError(500, ErrorUtil.getMsg(e), resp);
         return null;
     }
-
-
-    /**
-     *  拦截MethodArgumentNotValidException类的异常
-     * @param e exception
-     * @return Result
-     */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseBody
-    public Result exceptionHandler(MethodArgumentNotValidException e) throws NullPointerException{
-        FieldError fieldError = e.getBindingResult().getFieldError();
-        assert fieldError != null;
-        String defaultMessage = fieldError.getDefaultMessage();
-        return Result.genFail(defaultMessage);
-    }
-
-
-
-
 
 }
--- src/main/java/com/sjw/permission/modules/sys/controller/TestController.java
+++ src/main/java/com/sjw/permission/modules/sys/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.sjw.permission.modules.sys.controller;
 
+import com.sjw.permission.modules.sys.param.TestVo;
 import io.swagger.annotations.Api;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,12 @@ public class TestController {
     public String hello() {
         return "Hello World !";
     }
+
+    @RequestMapping(value = "/hello1", method = RequestMethod.GET)
+    public String hello1(@Validated TestVo testVo) {
+        return "Hello World !";
+    }
+
 }
 
 
